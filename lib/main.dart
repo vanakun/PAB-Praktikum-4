e715cf4c14a45dd52257d96a82d69cacd3ff7003
Dava Nabila <davanabila@Davas-MacBook-Pro.local>
2023-11-30 22:53:01 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+//Dava Nabila Muzaky 1462100153
+
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:praktikum_3/auth/register.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Apps',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Register(),
+      },
+    );
+  }
+}
+
